f4ef5aa70ab6ac37fafe29fccea6a91857b29c4d
Jasmontas Lukas <lukas.jasmontas@mif.stud.vu.lt>
2017-09-02 18:26:22 +0300

    Added price on image

--- coffee-price-list/src/App.js
+++ coffee-price-list/src/App.js
@@ -3,10 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import CoffeeList from './CoffeeList';
 
-var data = [{ id: 0, name: "Latte", image: "http://sawadacoffee.com/wp-content/uploads/Sawada-Coffee-10DEC2015-003.jpg" },
-{ id: 1, name: "Dark Coffee", image: "http://www.legalreader.com/wp-content/uploads/2015/10/Coffee-Cup-Bowl-With-Grains-Images.jpg" },
-{ id: 2, name: "Cappuccino", image: "https://www.nespresso.com/ncp/res/uploads/recipes/377c71f2069b1dba47f10aeb701d576d889101ee.jpg" },
-{ id: 3, name: "Milk Coffee", image: "http://2fhjuw36qc3g36anx54adxd2nje.wpengine.netdna-cdn.com/wp-content/uploads/2015/09/Coffee-6.png" }];
+var data = [{ id: 0, name: "Latte", price:"10", image: "http://sawadacoffee.com/wp-content/uploads/Sawada-Coffee-10DEC2015-003.jpg" },
+{ id: 1, name: "Dark Coffee", price:"7", image: "http://www.legalreader.com/wp-content/uploads/2015/10/Coffee-Cup-Bowl-With-Grains-Images.jpg" },
+{ id: 2, name: "Cappuccino", price:"5", image: "https://www.nespresso.com/ncp/res/uploads/recipes/377c71f2069b1dba47f10aeb701d576d889101ee.jpg" },
+{ id: 3, name: "Milk Coffee", price:"8.8", image: "http://2fhjuw36qc3g36anx54adxd2nje.wpengine.netdna-cdn.com/wp-content/uploads/2015/09/Coffee-6.png" },
+{ id: 4, name: "Milk Coffee", price:"8.8", image: "http://2fhjuw36qc3g36anx54adxd2nje.wpengine.netdna-cdn.com/wp-content/uploads/2015/09/Coffee-6.png" }];
 
 class App extends Component {
   render() {
@@ -17,7 +18,7 @@ class App extends Component {
           <h2>Coffee price list</h2>
         </div>
         <div className="App-intro">
-          <div className="row Coffee-list">
+          <div className="row">
             <CoffeeList data={data} />
           </div>
         </div>
--- coffee-price-list/src/CoffeeList.js
+++ coffee-price-list/src/CoffeeList.js
@@ -6,7 +6,10 @@ class CoffeeList extends React.Component {
             {this.props.data.map(function (item) {
                 return <div className="col-xs-3" key={item.id}>
                     <div className="Coffee-element">
-                        <img className="Coffee-image" src={item.image} alt={item.name} />
+                        <div className="Image-block">
+                            <img className="Coffee-image" src={item.image} alt={item.name} />
+                            <span className="Coffee-price">{item.price} Eur</span>
+                        </div>
                         <div className="Coffee-name">{item.name}</div>
                     </div>
                 </div>
